ba26c961af77c239ded61753350c70d121ee3886
Artur <arturremizov@gmail.com>
2022-12-15 18:01:13 +0300

    animate Custom shapes with AnimateableData

---
+++ SwiftfulThinkingAdvancedLearning/AnimateableDataBootcamp.swift
@@ -0,0 +1,87 @@
+//
+//  AnimateableDataBootcamp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Artur Remizov on 15.12.22.
+//
+
+import SwiftUI
+
+struct AnimateableDataBootcamp: View {
+    
+    @State private var animate: Bool = false
+    
+    var body: some View {
+        ZStack {
+//            RoundedRectangle(cornerRadius: animate ? 60 : 0)
+//            RectangleWithSingleCornerAnimation(cornerRadius: animate ? 60 : 0)
+            Pacman(offsetAmount: animate ? 20 : 0)
+                .frame(width: 250, height: 250)
+        }
+        .onAppear {
+            withAnimation(.easeInOut.repeatForever()) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+struct AnimateableDataBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimateableDataBootcamp()
+    }
+}
+
+struct RectangleWithSingleCornerAnimation: Shape {
+    
+    var cornerRadius: CGFloat
+    
+    var animatableData: CGFloat {
+        get { cornerRadius }
+        set { cornerRadius = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerRadius))
+            
+            path.addArc(
+                center: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY - cornerRadius),
+                radius: cornerRadius,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 180),
+                clockwise: false
+            )
+            
+            path.addLine(to: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
+}
+
+
+struct Pacman: Shape {
+    
+    var offsetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { offsetAmount }
+        set { offsetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let center = CGPoint(x: rect.midX, y: rect.midY)
+            path.move(to: center)
+            path.addArc(
+                center: center,
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: offsetAmount),
+                endAngle: Angle(degrees: 360 - offsetAmount),
+                clockwise: false
+            )
+        }
+    }
+}
